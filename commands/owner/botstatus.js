a20b8f56ee0275d42012cd49a6b89461791aa460
AdamSkI2003 <60717729+AdamK2003@users.noreply.github.com>
2021-05-15 16:36:48 +0200

    add botstatus

---
+++ commands/owner/botstatus.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+
+let allowedStatus = ['online','idle','invisible','dnd']
+let allowedActivity = ['PLAYING','WATCHING','LISTENING','STREAMING','COMPETING']
+let status, activity, activityName
+module.exports.help = {
+  name: "rank",
+  aliases: ["level"]
+}
+
+module.exports.run = async (client, message, args) => {
+    if(message.author.id != '206125763328606208') return
+    status = allowedStatus.includes(args[1].toLowerCase()) ? args[1].toLowerCase() : 'online'
+    activity = allowedActivity.includes(args[2].toUpperCase()) ? args[2].toUpperCase() : 'PLAYING'
+    activityName = args.slice(3).join(' ')
+
+    client.user.setPresence({
+    status: status,
+    activity: {
+        name: activityName,
+        type: activity
+    }
+    })
+}
